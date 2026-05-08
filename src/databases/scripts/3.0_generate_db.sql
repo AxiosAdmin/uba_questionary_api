ec23640c10ad92084f91df61569e518c7b8dcd8d
Pedro Vieira <pedro515vieira@gmail.com>
2026-05-08 17:52:15 -0300

    [bugfix] - Adjusting relation to the db script creation with the project models

--- src/databases/scripts/3.0_generate_db.sql
+++ src/databases/scripts/3.0_generate_db.sql
@@ -1,95 +1,132 @@
 CREATE TABLE IF NOT EXISTS institutions (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NULL
-);
-
-CREATE TABLE IF NOT EXISTS questions (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    institution_id uuid NOT NULL REFERENCES institutions(id),
-    topic character varying(100) NOT NULL,
-    subtopic text NOT NULL,
-    subtopic_description text NOT NULL,
-    diversity_mode text NOT NULL,
-    question text NOT NULL,
-    answer_a text NOT NULL,
-    answer_b text NOT NULL,
-    answer_c text NOT NULL,
-    answer_d text NOT NULL,
-    answer_e text,
-    explanation_a text NOT NULL,
-    explanation_b text NOT NULL,
-    explanation_c text NOT NULL,
-    explanation_d text NOT NULL,
-    explanation_e text,
-    correct_answer character(1) NOT NULL CHECK (correct_answer IN ('A', 'B', 'C', 'D', 'E')),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NULL
-);
-
-CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name text NOT NULL,
-    email text NOT NULL,
-    nickname text NOT NULL,
-    password text NOT NULL,
-    global_role character varying(50) DEFAULT 'User' NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NULL
-);
-
-CREATE TABLE IF NOT EXISTS favorite_questions (
-    user_id uuid NOT NULL REFERENCES users(id),
-    question_id uuid NOT NULL REFERENCES questions(id),
-    PRIMARY KEY (user_id, question_id)
-);
-
-CREATE TABLE IF NOT EXISTS question_answers (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id uuid NOT NULL REFERENCES users(id),
-    question_id uuid NOT NULL REFERENCES questions(id),
-    answer character(1) NOT NULL CHECK (answer IN ('A', 'B', 'C', 'D', 'E')),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NULL
-);
-
-CREATE TABLE IF NOT EXISTS question_feedbacks (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    question_id uuid NOT NULL REFERENCES questions(id),
-    is_liked boolean NOT NULL,
-    feedback character varying(100) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NULL
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT institutions_pkey PRIMARY KEY (id),
+    CONSTRAINT institutions_name_key UNIQUE (name)
 );
 
 CREATE TABLE IF NOT EXISTS profiles (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(50) NOT NULL UNIQUE,
-    questions_create_limit integer,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NULL
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    questions_create_limit INTEGER,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT profiles_pkey PRIMARY KEY (id),
+    CONSTRAINT profiles_name_key UNIQUE (name)
 );
 
-CREATE TABLE IF NOT EXISTS users_institutions (
-    user_id uuid NOT NULL REFERENCES users(id),
-    institution_id uuid NOT NULL REFERENCES institutions(id),
-    profile_id uuid NOT NULL REFERENCES profiles(id),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NULL,
-    PRIMARY KEY (user_id, institution_id)
+CREATE TABLE IF NOT EXISTS users (
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    nickname TEXT NOT NULL,
+    password TEXT NOT NULL,
+    global_role VARCHAR(50) DEFAULT 'User'::character varying NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS questions (
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    institution_id UUID NOT NULL,
+    topic VARCHAR(100) NOT NULL,
+    subtopic TEXT NOT NULL,
+    subtopic_description TEXT NOT NULL,
+    diversity_mode TEXT NOT NULL,
+    question TEXT NOT NULL,
+    answer_a TEXT NOT NULL,
+    answer_b TEXT NOT NULL,
+    answer_c TEXT NOT NULL,
+    answer_d TEXT NOT NULL,
+    explanation_a TEXT NOT NULL,
+    explanation_b TEXT NOT NULL,
+    explanation_c TEXT NOT NULL,
+    explanation_d TEXT NOT NULL,
+    correct_answer CHAR(1) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    answer_e TEXT,
+    explanation_e TEXT,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT questions_pkey PRIMARY KEY (id),
+    CONSTRAINT questions_correct_answer_check CHECK (
+        correct_answer = ANY (ARRAY['A'::bpchar, 'B'::bpchar, 'C'::bpchar, 'D'::bpchar, 'E'::bpchar])
+    ),
+    CONSTRAINT questions_institution_id_fkey FOREIGN KEY (institution_id) REFERENCES institutions (id)
 );
 
 CREATE TABLE IF NOT EXISTS subscriptions (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES users(id),
-    stripe_subscription_id TEXT UNIQUE NOT NULL,
-    stripe_customer_id TEXT,
-    status VARCHAR(30) NOT NULL CHECK (status IN ( 'active', 'failed_payment', 'canceled', 'incomplete', 'trialing')),
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    user_id UUID NOT NULL,
+    stripe_subscription_id TEXT NOT NULL,
+    status VARCHAR(30) NOT NULL,
     price_id TEXT NOT NULL,
-    current_period_end TIMESTAMP,
-    questions_generated_in_cycle INTEGER NOT NULL DEFAULT 0,
-    questions_generation_cycle_end TIMESTAMP,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    stripe_customer_id TEXT,
+    current_period_end TIMESTAMP WITHOUT TIME ZONE,
+    questions_generated_in_cycle INTEGER DEFAULT 0 NOT NULL,
+    questions_generation_cycle_end TIMESTAMP WITHOUT TIME ZONE,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT subscriptions_pkey PRIMARY KEY (id),
+    CONSTRAINT subscriptions_status_check CHECK (
+        status::text = ANY (
+            ARRAY[
+                'active'::character varying,
+                'failed_payment'::character varying,
+                'canceled'::character varying,
+                'incomplete'::character varying,
+                'trialing'::character varying
+            ]::text[]
+        )
+    ),
+    CONSTRAINT subscriptions_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT subscriptions_stripe_subscription_id_key UNIQUE (stripe_subscription_id)
+);
+
+CREATE TABLE IF NOT EXISTS users_institutions (
+    user_id UUID NOT NULL,
+    institution_id UUID NOT NULL,
+    profile_id UUID NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT users_institutions_pkey PRIMARY KEY (user_id, institution_id),
+    CONSTRAINT users_institutions_institution_id_fkey FOREIGN KEY (institution_id) REFERENCES institutions (id),
+    CONSTRAINT users_institutions_profile_id_fkey FOREIGN KEY (profile_id) REFERENCES profiles (id),
+    CONSTRAINT users_institutions_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS favorite_questions (
+    user_id UUID NOT NULL,
+    question_id UUID NOT NULL,
+    CONSTRAINT favorite_questions_pkey PRIMARY KEY (user_id, question_id),
+    CONSTRAINT favorite_questions_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions (id),
+    CONSTRAINT favorite_questions_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS question_answers (
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    user_id UUID NOT NULL,
+    question_id UUID NOT NULL,
+    answer CHAR(1) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT question_answers_pkey PRIMARY KEY (id),
+    CONSTRAINT question_answers_answer_check CHECK (
+        answer = ANY (ARRAY['A'::bpchar, 'B'::bpchar, 'C'::bpchar, 'D'::bpchar, 'E'::bpchar])
+    ),
+    CONSTRAINT question_answers_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions (id),
+    CONSTRAINT question_answers_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS question_feedbacks (
+    id UUID DEFAULT gen_random_uuid() NOT NULL,
+    question_id UUID NOT NULL,
+    is_liked BOOLEAN NOT NULL,
+    feedback VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT question_feedbacks_pkey PRIMARY KEY (id),
+    CONSTRAINT question_feedbacks_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions (id)
 );
