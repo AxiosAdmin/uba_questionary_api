c4a2e7a756191237d48e88568139c639a12ef1e6
Pedro Vieira <pedro515vieira@gmail.com>
2026-05-08 17:54:28 -0300

    [bugfix] - Adjustments on drop db file

--- src/databases/scripts/drop_db.sql
+++ src/databases/scripts/drop_db.sql
@@ -1,5 +1,6 @@
 -- Script para dropar as tabelas do banco de dados
 
+DROP TABLE IF EXISTS subscriptions;
 DROP TABLE IF EXISTS users_institutions;
 DROP TABLE IF EXISTS question_feedbacks;
 DROP TABLE IF EXISTS favorite_questions;
