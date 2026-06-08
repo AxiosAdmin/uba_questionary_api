0a8f38f418cc7b21506995381be092ca2e96c239
Pedro Vieira <pedro515vieira@gmail.com>
2026-06-08 14:19:17 -0300

    [bugfix] - DB Scripts adjustments and added admin application to CORS validation

--- src/configs/configs.py
+++ src/configs/configs.py
@@ -51,6 +51,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://ubaquestionaryfront-production.up.railway.app",
+        "https://ubaquestionaryfrontadm-production-feeb.up.railway.app",
         "https://axiosacademia.com",
     ]
 
--- src/databases/scripts/seeders/adm_user.sql
+++ src/databases/scripts/seeders/adm_user.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (name, nickname, password, global_role) VALUES
-('gAAAAABp7mCJaLYWOMDgEuRGXem8SG2lYJGkgqj69CguXKsV5-0rAnQF_yAWXmFvOLJifmOETmewZuk0iAOqpIW_R4Re01G088wTk6sHqds8tE2vgGMOYVM=', 'gAAAAABp7mCJXackwYs1SUgVuCZCQlTLeizUhIONfSmvEH88qxnt82YsIUOMGdJuB8nxSIV5vAIblS3lL0xngytadZou1FaU1Q==', 'gAAAAABp7mCJu3jlT0z1mJl0DM3axcgT6jmbaVZkpU816s_DQ8XuVAu3jg4xoHo1ifelnWW2yM9lyrHRrDzXLNmOMR8Z-0gVyg==', 'Admin');
+INSERT INTO users (name, email, email_hash, nickname, nickname_hash, dni, dni_hash, password, global_role, stripe_customer_id, created_at, updated_at)
+VALUES  ('gAAAAABqIcXbY9ymbrxBF8GzbW710XZU-anpipAqMdNdupK7hpdnLgSJeo5gg_zqkeYIPlSrPuNpucHgn7YQ5Cih7fTeAtgQ1A==', 'gAAAAABqIcXbbIEEmjjhL4fl7Rbo7Gn1PhIIGjL0uSg7OfMgIoqS22kvwcRpKURG3z7nJlBkRxZYrINMoU9_x_i0TFKwAXl1lA==', '51806ae91bbbd9e77fd42d439dc5eadbc4b2113926a21d0b9e008690682736d6', 'gAAAAABqIcXbljSchVQ6XmsumsWPZsDDDH8xpkHzX3VWuQElDpZO1S5LcH2gO_t2Mzetrb8N6qkPgJQlELkDaZZRQPvS2vNIhg==', 'c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f', 'gAAAAABqJu6_kb1HStbRlN0A668zrncX3w4Z9muSpmwdAVCLwd_ZcQ_eAsXzykL0uds-H3f161J9TRHtR7JCtj6-eR54CCSyGA==', 'e0cfc93ef472b6eb57227b299ca55532b7c5e0d229cf552cb8cb7ae1f230a99a', 'gAAAAABqIcXbiqQia5X-57PTGbaw8WFCMfGa86stG2YYKgQFIr_8WwAGPu93hQ6-kwuQDqQX08waNGwtaFqFa4FF1Q92rbYnow==', 'Admin', NULL, '2026-06-04 18:37:15.90212', NULL);
 
 INSERT INTO institutions (name) VALUES
 ('UBA');
