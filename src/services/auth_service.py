296b8846053c2530fb37d3e7e23cfabaa4182310
Pedro Vieira <pedro515vieira@gmail.com>
2026-07-02 00:44:02 -0300

    debugging logging error

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -118,6 +118,7 @@ class AuthService:
             ValueError: If nickname or password is invalid
         """
         user = await AuthService._find_user_by_nickname(nickname, db)
+        print(f"User found: {user.nickname if user else 'None'}")  # Debugging line
         if user and fernet_utils.decrypt(user.password) == password:
             if user.global_role == "Admin":
                 return user
@@ -126,6 +127,8 @@ class AuthService:
             user_institutions = await UserInstitutionService.read_user_institutions(
                 user.id, uba_institution.id, db
             )
+
+            print(f"User institutions: {user_institutions}")  # Debugging line
 
             if user_institutions:
                 return user_institutions
